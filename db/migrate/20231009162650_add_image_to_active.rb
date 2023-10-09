6c87b827694266a73cadd92580191016e11365ee
“shogo <shogo.yano1129@gmail.com>
2023-10-10 01:44:30 +0900

    kousin20231010

---
+++ db/migrate/20231009162650_add_image_to_active.rb
@@ -0,0 +1,5 @@
+class AddImageToActive < ActiveRecord::Migration[6.1]
+  def change
+    add_column :actives, :image, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_21_165755) do
+ActiveRecord::Schema.define(version: 2023_10_09_162650) do
 
   create_table "active_tag_relations", force: :cascade do |t|
     t.integer "active_id", null: false
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2023_09_21_165755) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.string "image"
   end
 
   create_table "comments", force: :cascade do |t|
